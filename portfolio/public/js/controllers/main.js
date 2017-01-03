2cff999875667761322d13994da4811fc2f2b335
William Hilton <williamjchilton@gmail.com>
2017-01-02 21:16:00 -0500

    ng-click added to about and contact

--- portfolio/public/js/controllers/main.js
+++ portfolio/public/js/controllers/main.js
@@ -9,3 +9,28 @@
 //     main.burgerOpen = false;
 //   }
 // }
+
+angular.module('portfolio')
+  .controller('MainController', MainController);
+
+MainController.$inject = ['$scope'];
+function MainController($scope) {
+  const main = this;
+
+  $scope.aboutClick = function() {
+    this.aboutShow = true;
+  };
+
+  $scope.contactClick = function() {
+    this.contactShow = true;
+  };
+
+  // $scope.hoverIn = function(){
+  //   this.hoverShow = true;
+  // };
+  //
+  // $scope.hoverOut = function(){
+  //   this.hoverShow = false;
+  // };
+
+}
