08f44a86ddf7218f02c49c433a7550deb3bfcd4d
William Hilton <williamjchilton@gmail.com>
2017-03-14 18:16:51 +0000

    fixed locationProvider bug

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 const router = require('./config/routes');
 const db = require('./config/db');
+const dest = `${__dirname}/public`; // added for res.sendFile
 
 let mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost/portfolio';
 mongoose.connect(mongoUri);
@@ -17,5 +18,6 @@ app.use(bodyParser.json());
 
 app.use(express.static(`${__dirname}/public`));
 app.use(router);
+app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`)); //added to make (non-existent)hashes work
 
 app.listen(port, () => console.log(`Express is listening on port ${port}`));
